865ce118171fd93a8945d75c27445af84b6283ec
mohammad sookhtesaraei <114998362+mohammadsookhtesaraei@users.noreply.github.com>
2025-06-22 19:08:32 +0330

    create pages folder and set AuthPage component

---
+++ src/components/templates/CheckOtpForm.jsx
@@ -0,0 +1,9 @@
+
+
+const CheckOtpForm = () => {
+  return (
+    <div>CheckOtpForm</div>
+  )
+}
+
+export default CheckOtpForm
---
+++ src/components/templates/SendOtpForm.jsx
@@ -0,0 +1,9 @@
+
+
+const SendOtpForm = () => {
+  return (
+    <div>SendOtpForm</div>
+  )
+}
+
+export default SendOtpForm
