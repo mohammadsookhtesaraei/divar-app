6320fe11f2284553ea856b9ab1c08b062a546135
mohammad sookhtesaraei <114998362+mohammadsookhtesaraei@users.noreply.github.com>
2025-06-26 10:20:10 +0330

    reactquery

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import { BrowserRouter } from "react-router";
 import Route from "./router/Router"
+
+import { QueryClient,QueryClientProvider } from "@tanstack/react-query";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools"
+
+import { BrowserRouter } from "react-router";
+
+import defaultOtions from "./configs/reactQuery";
 
 
 function App() {
+  const queryClient=new QueryClient({defaultOtions})
+
   return (
+    <QueryClientProvider client={queryClient}>
     <BrowserRouter>
      <Route/>
     </BrowserRouter>
+    <ReactQueryDevtools/>
+    </QueryClientProvider>
   )
 }
 
---
+++ src/configs/reactQuery.js
@@ -0,0 +1,12 @@
+
+const defaultOtions={
+       queries:{
+        refetchOnWindowFocus:false,
+        refetchOnMount:false,
+        retry:1,
+        staleTime:60 * 1000
+      }
+};
+
+
+export default defaultOtions;
