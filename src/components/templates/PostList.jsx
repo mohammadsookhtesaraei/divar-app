566d8c42c5a0abac5713068082d6bed3846cde3a
mohammad sookhtesaraei <114998362+mohammadsookhtesaraei@users.noreply.github.com>
2025-07-07 15:31:03 +0330

    added css for PostList component

--- src/components/templates/PostList.jsx
+++ src/components/templates/PostList.jsx
@@ -1,16 +1,44 @@
 
-import { useQuery } from "@tanstack/react-query"
+import { useQuery} from "@tanstack/react-query"
 import {getPosts} from "../../services/user"
+import {sp} from "../../utils/Number"
+
+import Loader from "../modules/Loader"
+
+import styles from "./PostList.module.css"
+
 
 const PostList = () => {
-    const {data,isPending}=useQuery({
+   
+    const {data,isPending,refetch}=useQuery({
         queryKey:["my-post-list"],
-        queryFn:getPosts
+        queryFn:getPosts,
+        
     });
+
+    console.log(data?.data.posts);
    
   return (
-    <div>
-
+    <div className={styles.list}>
+      {
+        isPending ? <Loader/> : (
+            <>
+            {data.data.posts.map((post)=>(
+                <div key={post._id} className={styles.post}>
+                    <img src={`${import.meta.env.VITE_BASE_URL}${post.images}`} />
+                     <div>
+                        <p>{post.options.title}</p>
+                        <span>{post.options.content}</span>
+                     </div>
+                     <div className={styles.price}>
+                        <p>{new Date(post.createdAt).toLocaleDateString("fa-IR")}</p>
+                        <span>{sp(post.amount)}</span>
+                     </div>
+                </div>
+            ))}
+            </>
+        )
+      }
     </div>
   )
 }
--- src/services/user.js
+++ src/services/user.js
@@ -3,6 +3,7 @@ import api from "../configs/api";
 
 
 const getProfile=()=>api.get("user/whoami").then((response)=> response || false);
-const getPosts=()=>api.get("post/my")
+const getPosts=()=>api.get("post/my");
+const deletePost=(id)=>api.delete(`post/delete/${id}`)
 
-export {getProfile,getPosts}
+export {getProfile,getPosts,deletePost}
