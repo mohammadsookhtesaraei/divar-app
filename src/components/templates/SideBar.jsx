7dd0f960bdba4c7f21918123ec88f2d511225eff
mohammad sookhtesaraei <114998362+mohammadsookhtesaraei@users.noreply.github.com>
2025-07-09 09:54:19 +0330

    finish sideBar

--- src/components/templates/SideBar.jsx
+++ src/components/templates/SideBar.jsx
@@ -1,8 +1,25 @@
+import { useQuery } from "@tanstack/react-query";
+import { getCategory } from "../../services/admin";
 
+import styles from "./SideBar.module.css";
 
 const SideBar = () => {
+      const { data } = useQuery({
+    queryKey: ["get-category"],
+    queryFn: getCategory,
+  });
   return (
-    <div>SideBar</div>
+    <div className={styles.sidebar}>
+        <h4>دسته ها</h4>
+        <ul>
+            {data?.data.map((category)=>(
+                <li key={category._id}>
+                    <img src={`${category.icon}.svg`} />
+                    <p>{category.name}</p>
+                </li>
+            ))}
+        </ul>
+    </div>
   )
 }
 
